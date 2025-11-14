d71317a0b5fe09cc26591c61c6000c2d7f6e6256
Alexander Lalejini <amlalejini@gmail.com>
2025-11-14 12:29:50 -0500

    Update run-gen-slurm-msu script

--- experiments/2025-08-27-env-chg/hpc/run-gen-slurm-msu.sh
+++ experiments/2025-08-27-env-chg/hpc/run-gen-slurm-msu.sh
@@ -16,9 +16,10 @@ REPO_DIR=/mnt/home/${USERNAME}/devo_ws/${PROJECT_NAME}
 REPO_SCRIPTS_DIR=${REPO_DIR}/scripts
 HOME_EXP_DIR=${REPO_DIR}/experiments/${EXP_SLUG}
 
-DATA_DIR=/mnt/scratch/${USERNAME}_scratch/${PROJECT_NAME}/${EXP_SLUG}
+DATA_DIR=/mnt/scratch/${USERNAME}/${PROJECT_NAME}/${EXP_SLUG}
 JOB_DIR=${DATA_DIR}/jobs
 CONFIG_DIR=${HOME_EXP_DIR}/hpc/config
+HPC_ENV_FILEPATH=${REPO_DIR}/hpc-env/${HPC_ENV_FILE}
 
 # (1) Activate appropriate Python virtual environment
 source ${REPO_DIR}/hpc-env/${HPC_ENV_FILE}
