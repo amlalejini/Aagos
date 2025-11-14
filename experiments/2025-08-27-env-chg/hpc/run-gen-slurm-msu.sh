1fa44250e3e09285c59324c692466ad002675f75
Alexander Lalejini <amlalejini@gmail.com>
2025-11-14 12:36:43 -0500

    Adjust path

--- experiments/2025-08-27-env-chg/hpc/run-gen-slurm-msu.sh
+++ experiments/2025-08-27-env-chg/hpc/run-gen-slurm-msu.sh
@@ -16,7 +16,7 @@ REPO_DIR=/mnt/home/${USERNAME}/devo_ws/${PROJECT_NAME}
 REPO_SCRIPTS_DIR=${REPO_DIR}/scripts
 HOME_EXP_DIR=${REPO_DIR}/experiments/${EXP_SLUG}
 
-DATA_DIR=/mnt/scratch/${USERNAME}/${PROJECT_NAME}/${EXP_SLUG}
+DATA_DIR=/mnt/scratch/${USERNAME}/data/${PROJECT_NAME}/${EXP_SLUG}
 JOB_DIR=${DATA_DIR}/jobs
 CONFIG_DIR=${HOME_EXP_DIR}/hpc/config
 HPC_ENV_FILEPATH=${REPO_DIR}/hpc-env/${HPC_ENV_FILE}
