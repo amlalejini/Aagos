ee8dccb4446d8c8987bfc393dda406c8261a9a09
Alexander Lalejini <amlalejini@gmail.com>
2026-01-05 11:22:17 -0500

    Draft 2026-01-05 experiment

---
+++ experiments/2026-01-05-no-reduced-mut-load/hpc/local-run-gen-slurm.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+REPLICATES=100
+EXP_SLUG=2026-01-05-no-reduced-mut-load
+SEED_OFFSET=50000
+JOB_TIME=02:00:00
+JOB_MEM=8G
+PROJECT_NAME=Aagos
+RUNS_PER_SUBDIR=1000
+USERNAME=lalejini
+# HPC_ENV_FILE=clipper-hpc-env.sh
+HPC_ENV_FILE=msu-hpc-env.sh
+ACCOUNT=devolab
+
+REPO_DIR=/Users/lalejina/devo_ws/${PROJECT_NAME}
+HOME_EXP_DIR=${REPO_DIR}/experiments/${EXP_SLUG}
+
+DATA_DIR=${HOME_EXP_DIR}/hpc/test/data
+JOB_DIR=${HOME_EXP_DIR}/hpc/test/jobs
+CONFIG_DIR=${HOME_EXP_DIR}/hpc/config
+
+# (1) Activate appropriate Python virtual environment
+source ${REPO_DIR}/pyenv/bin/activate
+
+# (2) Generate slurm script
+#   - This will generate an events file for each run
+python3 gen-slurm.py \
+  --runs_per_subdir ${RUNS_PER_SUBDIR} \
+  --time_request ${JOB_TIME} \
+  --mem ${JOB_MEM} \
+  --data_dir ${DATA_DIR} \
+  --config_dir ${CONFIG_DIR} \
+  --repo_dir ${REPO_DIR} \
+  --replicates ${REPLICATES} \
+  --job_dir ${JOB_DIR} \
+  --seed_offset ${SEED_OFFSET}
---
+++ experiments/2026-01-05-no-reduced-mut-load/hpc/run-gen-slurm.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+REPLICATES=100
+EXP_SLUG=2026-01-05-no-reduced-mut-load
+SEED_OFFSET=50000
+JOB_TIME=02:00:00
+JOB_MEM=8G
+PROJECT_NAME=Aagos
+RUNS_PER_SUBDIR=1000
+USERNAME=lalejini
+# HPC_ENV_FILE=clipper-hpc-env.sh
+HPC_ENV_FILE=msu-hpc-env.sh
+ACCOUNT=devolab
+
+REPO_DIR=/mnt/home/${USERNAME}/devo_ws/${PROJECT_NAME} # <-- CHANGE THIS to where ever you have this repository stored on your account
+REPO_SCRIPTS_DIR=${REPO_DIR}/scripts
+HOME_EXP_DIR=${REPO_DIR}/experiments/${EXP_SLUG}
+
+DATA_DIR=/mnt/scratch/${USERNAME}/data/${PROJECT_NAME}/${EXP_SLUG}
+JOB_DIR=${DATA_DIR}/jobs
+CONFIG_DIR=${HOME_EXP_DIR}/hpc/config
+HPC_ENV_FILEPATH=${REPO_DIR}/hpc-env/${HPC_ENV_FILE}
+
+# (1) Activate appropriate Python virtual environment
+source ${HPC_ENV_FILEPATH}
+source ${REPO_DIR}/pyenv/bin/activate
+
+# (2) Generate slurm script
+#   - This will generate an events file for each run
+python3 gen-slurm.py \
+  --runs_per_subdir ${RUNS_PER_SUBDIR} \
+  --time_request ${JOB_TIME} \
+  --mem ${JOB_MEM} \
+  --data_dir ${DATA_DIR} \
+  --config_dir ${CONFIG_DIR} \
+  --repo_dir ${REPO_DIR} \
+  --replicates ${REPLICATES} \
+  --job_dir ${JOB_DIR} \
+  --seed_offset ${SEED_OFFSET} \
+  --hpc_account ${ACCOUNT} \
+  --hpc_env_file ${HPC_ENV_FILEPATH}
